4395e5596e25935dbdee6efcc08c52a33c83bba8
GovindKalyanaMOJ <govindaraju.kalyanaraman@digital.justice.gov.uk>
2018-08-08 13:49:49 +0100

    Initial commit

---
+++ src/main/java/com/laa/nolasa/laanolasa/LaaNolasaApplication.java
@@ -0,0 +1,12 @@
+package com.laa.nolasa.laanolasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaaNolasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaaNolasaApplication.class, args);
+	}
+}
