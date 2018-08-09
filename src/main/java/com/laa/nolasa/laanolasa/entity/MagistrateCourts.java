65f12e57476ca10dfa5da74a0ccc84de1da6c03f
GovindKalyanaMOJ <govindaraju.kalyanaraman@digital.justice.gov.uk>
2018-08-09 15:36:47 +0100

    ojdbc jar and application.yml configuration for JDBC done

--- src/main/java/com/laa/nolasa/laanolasa/entity/MagistrateCourts.java
+++ src/main/java/com/laa/nolasa/laanolasa/entity/MagistrateCourts.java
@@ -3,6 +3,7 @@ package com.laa.nolasa.laanolasa.entity;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Column;
 
@@ -11,6 +12,7 @@ import javax.persistence.Column;
 @Table(name = "MAGISTRATE_COURTS", schema = "TOGDATA")
 public class MagistrateCourts {
 
+    @Id
     @Column(name = "COURT", updatable = false, nullable = false)
     private String court;
 
