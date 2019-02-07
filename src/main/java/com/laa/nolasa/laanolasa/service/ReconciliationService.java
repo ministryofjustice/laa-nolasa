e17b678e52d5f4296e25da7db4c0d1cf00d5c77f
Mat Moore <matmoore@users.noreply.github.com>
2019-02-07 11:30:23 +0000

    Count each outcome of processing a NOLASA query
    - The matches were the same as ones the user already rejected
    - Libra returned no matches
    - Libra returned one match
    - Libra returned many matches
    - There was an error querying Libra or saving the results
    
    I think counters make sense for these metrics because we want to know
    the cases processed over time, rather than the cases processed by a
    particular task run. We should care more about the ratios between these
    things than the absolute numbers, because the number of cases processed
    in a particular time period depends on how frequently we run the task
    and whether there were errors in the previous run.
    
    Once this data is being recorded, we should be able to graph the % of
    cases in each category over time using some metric math
    (https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/using-metric-math.html)

--- src/main/java/com/laa/nolasa/laanolasa/service/ReconciliationService.java
+++ src/main/java/com/laa/nolasa/laanolasa/service/ReconciliationService.java
@@ -31,14 +31,25 @@ public class ReconciliationService {
 
     private InfoXServiceClient infoXServiceClient;
     private NolRepository nolRepository;
-    private Counter failedQueries;
-    private Counter successfulQueries;
+    private Counter singleMatchQueries;
+    private Counter multiMatchQueries;
+    private Counter noMatchQueries;
+    private Counter errorQueries;
+    private Counter alreadyRejectedQueries;
     private DistributionSummary numberOfResults;
 
     public ReconciliationService(NolRepository nolRepository, InfoXServiceClient infoXService) {
         this.nolRepository = nolRepository;
         this.infoXServiceClient = infoXService;
-        this.successfulQueries = Metrics.globalRegistry.counter("reconciliation.successful");
+
+        // Counts of queries with each outcome
+        this.singleMatchQueries = Metrics.globalRegistry.counter("queries.singleMatch");
+        this.multiMatchQueries = Metrics.globalRegistry.counter("queries.multiMatch");
+        this.noMatchQueries = Metrics.globalRegistry.counter("queries.noMatch");
+        this.errorQueries = Metrics.globalRegistry.counter("queries.error");
+        this.alreadyRejectedQueries = Metrics.globalRegistry.counter("queries.alreadyRejected");
+
+        // The number of results returned by Libra (a number between 0 and MAX_LIBRA_RECORDS)
         this.numberOfResults = Metrics.globalRegistry.summary("reconciliation.numberOfResults");
     }
 
@@ -48,27 +59,43 @@ public class ReconciliationService {
 
         log.info("Retrieved libra {} entities from db", notInLibraEntities.size());
 
-        notInLibraEntities.stream().forEach(entity -> {
+        notInLibraEntities.stream().forEach(this::reconcileNolRecord);
+    }
 
+    private void reconcileNolRecord(Nol entity) {
+        Long maatId = entity.getRepOrders().getId();
+
+        try {
             InfoXSearchResult infoXSearchResult = infoXServiceClient.search(entity);
+            int numberOfResults = infoXSearchResult.getLibraIDs().length;
+            this.numberOfResults.record(numberOfResults);
 
             if (InfoXSearchStatus.FAILURE == infoXSearchResult.getStatus()) {
-                log.info("No matching record returned by infoX service for MAATID {}", entity.getRepOrders().getId());
+                log.info("No matching record returned by infoX service for MAATID {}", maatId);
+                this.noMatchQueries.increment();
             } else if (NolStatuses.RESULTS_REJECTED.valueOf().equals(entity.getStatus()) && areLibraIDsEqual(entity.getRepOrders().getNolAutoSearchResults(), infoXSearchResult.getLibraIDs())) {
-                log.info("Results were previously rejected, no changes are detected in libra IDs corresponding to the MAAT ID: {} ", entity.getRepOrders().getId());
+                log.info("Results were previously rejected, no changes are detected in libra IDs corresponding to the MAAT ID: {} ", maatId);
+                this.alreadyRejectedQueries.increment();
             } else {
-                int numberOfResults = infoXSearchResult.getLibraIDs().length;
-                this.numberOfResults.record(numberOfResults);
-
                 updateNol(entity, infoXSearchResult);
+
                 if (dryRunMode) {
-                    log.info("Dry run mode - so no changes made to the database for MAAT ID {}", entity.getRepOrders().getId());
+                    log.info("Dry run mode - so no changes made to the database for MAAT ID {}", maatId);
                 } else {
                     nolRepository.save(entity);
-                    log.info("Status for MAAT ID {} has been updated to 'RESULTS FOUND'", entity.getRepOrders().getId());
+                    log.info("Status for MAAT ID {} has been updated to 'RESULTS FOUND'", maatId);
+                }
+
+                if (numberOfResults == 1) {
+                    this.singleMatchQueries.increment();
+                } else {
+                    this.multiMatchQueries.increment();
                 }
             }
-        });
+        } catch (Exception e) {
+            log.error("Error handling MAATID " + maatId + " - skipping", e);
+            this.errorQueries.increment();
+        }
     }
 
     void updateNol(Nol entity, InfoXSearchResult infoXSearchResult) {
@@ -87,8 +114,6 @@ public class ReconciliationService {
         entity.setStatus(NolStatuses.RESULTS_FOUND.valueOf());
         entity.setDateLastModified(LocalDateTime.now());
         entity.setUserLastModified("NOLASA");
-
-        this.successfulQueries.increment();
     }
 
 }
