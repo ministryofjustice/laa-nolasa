a2b5b63e103bfb7888f11134160fbda9a2ccf027
Mian-MOJ <mian.mahmood@digital.justice.gov.uk>
2019-02-12 10:57:07 +0000

    Included count 1 scenario to fix unit test
    Unit test failed so included the missing scenario of count = 1

--- src/main/java/com/laa/nolasa/laanolasa/common/ReconciliationResult.java
+++ src/main/java/com/laa/nolasa/laanolasa/common/ReconciliationResult.java
@@ -11,6 +11,7 @@ public enum ReconciliationResult {
     public static ReconciliationResult fromCount(Long count) {
         if (count < 0) return ERROR;
         if (count == 0) return NO_MATCHES;
+        if (count == 1) return ONE_MATCH;
         if (count > 15) return OVER_15_MATCHES;
         return MANY_MATCHES;
     }
