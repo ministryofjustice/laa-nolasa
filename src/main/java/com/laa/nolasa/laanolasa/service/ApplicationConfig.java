6c932675eaf4190f2e4ab3c45995fcf3264c7307
Mian-MOJ <mian.mahmood@digital.justice.gov.uk>
2019-03-05 09:34:10 +0000

    Replaced Spring WS with Apache cxf for consuming Infox SOAP
    Spring WS implmentation of JAXB caused few problems with Java 11 module system, so had to replace with Apache CXF.
    
    There is no standard plugin to generate sources based on XSD / WSDL, currently using no.nils.wsdl2java. This works fine, however had to add large number of dependencies to support this in Java 11. This could be replaced in future with better plugin.
    
    Currently there are few problems around SOAP version and XML name issue which we fixed for JAXB but needs to be fixed for cxf implementation.

--- src/main/java/com/laa/nolasa/laanolasa/service/ApplicationConfig.java
+++ src/main/java/com/laa/nolasa/laanolasa/service/ApplicationConfig.java
@@ -2,20 +2,14 @@ package com.laa.nolasa.laanolasa.service;
 
 import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.sun.xml.bind.marshaller.NamespacePrefixMapper;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.ws.client.core.WebServiceTemplate;
-import org.springframework.ws.soap.SoapVersion;
-import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
-
-import java.util.HashMap;
-import java.util.Map;
+import uk.gov.justice._2013._11.magistrates.LIBRAServicePortType;
 
 @Configuration
 public class ApplicationConfig {
@@ -30,43 +24,16 @@ public class ApplicationConfig {
   }
 
   @Bean
-  Jaxb2Marshaller jaxb2Marshaller() {
-    Jaxb2Marshaller jaxb2Marshaller = new Jaxb2Marshaller();
-    jaxb2Marshaller.setContextPath("uk.gov.justice._2013._11.magistrates");
-    NamespacePrefixMapper mapper = new NamespacePrefixMapper() {
-      public String getPreferredPrefix(String namespaceUri, String suggestion, boolean requirePrefix) {
-        if ("http://www.justice.gov.uk/2013/11/magistrates".equals(namespaceUri) && !requirePrefix)
-          return "";
-        return "ns";
-      }
-    };
-    Map<String, Object> propertiesMap = new HashMap<>();
-    propertiesMap.put("jaxb.formatted.output", true);
-    propertiesMap.put("com.sun.xml.bind.namespacePrefixMapper", mapper);
-    jaxb2Marshaller.setMarshallerProperties(propertiesMap);
-
-    return jaxb2Marshaller;
-  }
-
-  @Bean
-  public WebServiceTemplate webServiceTemplate() {
-    WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
-    webServiceTemplate.setMarshaller(jaxb2Marshaller());
-    webServiceTemplate.setUnmarshaller(jaxb2Marshaller());
-    webServiceTemplate.setDefaultUri(defaultUri);
-    webServiceTemplate.setMessageFactory(soapMessageFactory());
-    return webServiceTemplate;
-  }
-
-  @Bean
-  public SaajSoapMessageFactory soapMessageFactory() {
-      SaajSoapMessageFactory saajSoapMessageFactory = new SaajSoapMessageFactory();
-      saajSoapMessageFactory.setSoapVersion(SoapVersion.SOAP_12);
-      return saajSoapMessageFactory;
-  }
-
-  @Bean
   public TimedAspect timedAspect(MeterRegistry registry) {
     return new TimedAspect(registry);
+  }
+
+  @Bean(name = "infoxProxy")
+  public LIBRAServicePortType helloWorldProxy() {
+    JaxWsProxyFactoryBean jaxWsProxyFactoryBean = new JaxWsProxyFactoryBean();
+    jaxWsProxyFactoryBean.setServiceClass(LIBRAServicePortType.class);
+    jaxWsProxyFactoryBean.setAddress(defaultUri);
+
+    return (LIBRAServicePortType) jaxWsProxyFactoryBean.create();
   }
 }
