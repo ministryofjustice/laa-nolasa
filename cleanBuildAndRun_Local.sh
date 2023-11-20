d75e3d7fec62af01a807dd6b11084b62fb673e82
Alex Stone <alex.stone@digital.justice.gov.uk>
2023-11-20 10:13:13 +0000

    Amend minor typo in comment

--- cleanBuildAndRun_Local.sh
+++ cleanBuildAndRun_Local.sh
@@ -5,7 +5,7 @@ set -ex
 # Get the directory of this script
 script_dir="$(dirname "$0")"
 
-# Change to that directory, which will be the root of laa-infoX-application
+# Change to that directory, which will be the root of this application (laa-nolasa)
 cd "$script_dir" || { echo "Unable to navigate to the root directory"; exit; }
 
 pwd
